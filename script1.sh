3a373668f42cc7be61162e243caf905a9e95c493
fdgdee <vitorrs97@outlook.com>
2025-06-27 23:30:24 -0300

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -7,7 +7,7 @@ fi
 #off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
 #dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 cp recovery.img r.img
-
+ls
 if [ ! -f phh.pem ];then
     openssl genrsa -f4 -out phh.pem 4096
 fi
